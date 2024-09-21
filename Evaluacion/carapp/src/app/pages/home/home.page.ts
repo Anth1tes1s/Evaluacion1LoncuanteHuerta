249456022b9505c29142c8e2a49e4aca486650a6
PLAYDEI <87295691+PLAYDEI@users.noreply.github.com>
2024-09-20 22:57:47 -0300

    primera animacion
    lo anime

--- Evaluacion/carapp/src/app/pages/home/home.page.ts
+++ Evaluacion/carapp/src/app/pages/home/home.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild  } from '@angular/core';
+import { AnimationController, IonTitle } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, AfterViewInit {
+  @ViewChild(IonTitle, { read: ElementRef})
+  ionTitleRef!: ElementRef<HTMLIonTitleElement>
 
-  constructor() { }
+  constructor(
+    private animationController: AnimationController
+  ) { }
 
   ngOnInit() {
   }
-
+  ngAfterViewInit(): void {
+    this.viewChildAnimation();
+  }
+  viewChildAnimation(){
+    this.animationController
+      .create()
+      .addElement(this.ionTitleRef.nativeElement)
+      .duration(3200)
+      .fromTo('transform', 'scale(0.3)', 'scale(1.2)')
+      .fromTo('color', 'white', '#477DFF')
+      .fromTo('opacity', '0.2', '1')
+      .play()
+  }
 }
