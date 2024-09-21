3e5b9ccd68dc009191ad5a899244c5662a20c3c7
PLAYDEI <87295691+PLAYDEI@users.noreply.github.com>
2024-09-20 23:29:16 -0300

    puse el boton atras

--- Evaluacion/carapp/src/app/pages/login/login.page.ts
+++ Evaluacion/carapp/src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild  } from '@angular/core';
+import { AnimationController, IonTitle } from '@ionic/angular';
 import { NavController } from '@ionic/angular'; // Para la navegación
 
 @Component({
@@ -6,12 +7,31 @@ import { NavController } from '@ionic/angular'; // Para la navegación
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage {
-
+export class LoginPage implements OnInit, AfterViewInit {
+  @ViewChild(IonTitle, { read: ElementRef})
+  ionTitleRef!: ElementRef<HTMLIonTitleElement>
   username: string = '';
   password: string = '';
 
-  constructor(private navCtrl: NavController) { }
+  constructor(
+    private navCtrl: NavController,
+    private animationController: AnimationController
+  ) { }
+  ngOnInit() {
+  }
+  ngAfterViewInit(): void {
+    this.tituloanimacion();
+  }
+  tituloanimacion(){
+    this.animationController
+      .create()
+      .addElement(this.ionTitleRef.nativeElement)
+      .duration(2000)
+      .fromTo('transform', 'scale(0.3)', 'scale(1.2)')
+      .fromTo('color', 'white', '#477DFF')
+      .fromTo('opacity', '0.2', '1')
+      .play()
+  }
 
   // Método para validar el login (mantén el código de validación aquí)
   validateLogin() {
@@ -23,3 +43,4 @@ export class LoginPage {
     this.navCtrl.navigateForward('/reset-password');
   }
 }
+
